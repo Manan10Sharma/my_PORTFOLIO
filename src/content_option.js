bed593fb1193b19d176a2b674820ba696beed4d4
Manan10Sharma <manansharma1151@gmail.com>
2025-07-22 04:35:57 +0530

    Clean commit without node_modules

---
+++ src/content_option.js
@@ -0,0 +1,134 @@
+const logotext = <b><strong>PORTFOLIO</strong></b>;
+const meta = {
+    title: "Manan Sharma",
+    description: "◡̈ I’m Manan Sharma - a Java Devloper, currently working in Bengaluru, India",
+};
+
+const introdata = {
+    title: "I’m Manan Sharma",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop softwares",
+    },
+    description: "Skilled in building scalable backend applications using Java and Spring Boot. Experienced with REST APIs, Spring MVC, and database integration using JPA/Hibernate.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Results-driven Java Developer with 2+ years of experience building scalable web applications using Java, Spring Boot, REST APIs, and Microservices. Proficient in React.js, MySQL, PostgreSQL, and tools like Git, Maven, Docker, and Postman. Strong grasp of Spring Security, JPA/Hibernate, and Agile methodology. Passionate about writing clean, testable code and delivering performant solutions in collaborative environments.",
+};
+const worktimeline = [{
+        jobtitle: "IT Executive and Developer",
+        where: "MedTermRCM Pvt. Ltd. | Raisinghnagar",
+        date: "Sep 2022 - Oct 2024",
+    },
+    {
+        jobtitle: "Full Stack Development Intern/Trainee – Java & Python",
+        where: "KodNest Ed-Tech. Pvt. Ltd. | Bengaluru",
+        date: "Dec 2024 - Jul 2025",
+    }
+];
+
+const skills = [{
+        name: "Java",
+        value: 85,
+    },
+    {
+        name: "Spring",
+        value: 75,
+    },
+    {
+        name: "Spring Boot",
+        value: 80,
+    },
+    {
+        name: "HTML",
+        value: 80,
+    },
+    {
+        name: "CSS",
+        value: 75,
+    },
+    {
+        name: "JavaScript",
+        value: 60,
+    },
+];
+
+const services = [{
+        title: "Java Developer",
+        description: <ul>
+            <p>Developed and maintained scalable web applications, resolving production issues and improving REST API performance by 40%. Collaborated with cross-functional teams on requirements, design, and implementation. Managed basic system administration with a focus on security, while adhering to SDLC standards and writing clean, maintainable code.</p>
+        </ul>
+    },
+    {
+        title: "Full Stack Development Intern/Trainee – Java & Python",
+        description: <ul>
+            <p>Completed comprehensive training in Java & Python Full Stack Development, including:</p>
+            <p>- Core Java, Advanced Java, Spring Boot, SQL, Web Development, Testing.</p>
+            <p>- Competitive Programming and software development best practices.</p>
+            <p>Focus on developing full-stack applications and gaining readiness for real- world technology roles.</p>
+        </ul>
+    
+    }
+];
+
+const dataportfolio = [
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "MailBot AI – Smart Email Replier - Java Full Stack | Spring Boot | React.js | Chrome Extension -- Built an AI-powered Gmail assistant to auto-generate tone-based replies (Professional, Casual, Friendly)",
+        link: "https://github.com/Manan10Sharma/MailBot_AI-Smart_Email_Replies",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "Employee Management System Java Full Stack | Spring Boot | Thymeleaf | MySQL -- Designed a web-based CRUD app for managing employee records with robust backend and UI features.",
+        link: "https://github.com/Manan10Sharma/Employee-Management-",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "CravZ – Online Food Delivery Platform Java Full Stack | Spring Boot | Thymeleaf | MySQL | Spring Security -- Developed a full-stack food delivery platform with secure user/admin features.",
+        link: "https://github.com/Manan10Sharma/CravZ",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Reserved for Future Project",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "Reserved for Future Project",
+        link: "#",
+    },   
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "manansharma597@gmail.com",
+    YOUR_FONE: "72319-77000",
+    description: "◡̈ Thank you for stepping into my world of ideas and code. Every click, every scroll, and every second spent here means a lot! Let’s create something amazing together.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Manan10Sharma",
+    instagram: "https://www.instagram.com/manansharma2002/",
+    linkedin: "https://www.linkedin.com/in/manan-sharma-7a827220a/",
+    twitter: "https://x.com/MananSharm2002",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
